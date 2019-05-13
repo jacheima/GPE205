9dab0d5cbdb4f7e2f0ddead867304d42593ecf5c
KNOWN-UNIVERSE\jacheima <jacheima@uat.edu>
2019-05-13 14:23:23 -0700

    We set up a pawn controller and input controller for the main player model in the tank game. I've decided to make a pirate ship sailing game.

---
+++ Tank_Game/Tank_Game/Assets/Scenes/InputController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    public Transform tf;
+    public Transform p1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+        p1 = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Start with the assumption that I am not moving
+        Vector3 directionToMove = Vector3.zero;
+
+        //If the W is pressed -- Add "forward to the direction I am moving
+        if (Input.GetKey(KeyCode.W))
+        {
+            directionToMove += Vector3.forward;
+        }
+
+        //If S key is down -- Add "reverse" to the direction I am moving
+
+
+
+
+        //after i've checked all my inputs, tell my mover to move in the final direction
+    }
+}
